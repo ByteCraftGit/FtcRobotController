7a447eacfcf42079eeff8f0e63ebe0f69603e63b
ByteCraftGit <suhanthokala@gmail.com>
2026-07-14 19:02:49 -0500

    more code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TouchSensorPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TouchSensorPractice.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.mechanisms.TestBench;
  @TeleOp
+ @Disabled
 public class TouchSensorPractice extends OpMode {
     TestBench bench = new TestBench();
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/TestBench.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/TestBench.java
@@ -1,16 +1,33 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class TestBench {
     private DigitalChannel touchSensor;
+    private DcMotor motor;
+    private double ticksPerRev;
 
     public void init(HardwareMap hwMap) {
+                // Dc Motor
+        motor = hwMap.get(DcMotor.class, "motor");
+        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        ticksPerRev = motor.getMotorType().getTicksPerRev();
+
+                    // Touch Sensor
         touchSensor = hwMap.get(DigitalChannel.class, "touch_sensor");
         touchSensor.setMode(DigitalChannel.Mode.INPUT);
     }
+                // ---Dc Motor---
+    public void setMotorSpeed(double speed) {
+        motor.setPower(speed);
+    }
 
+    public double getMotorRevs() {
+        return motor.getCurrentPosition() / ticksPerRev;
+    }
+            //---Touch Sensor---
     public boolean getTouchSensorState() {
         return  !touchSensor.getState();
     }
@@ -18,6 +35,8 @@ public class TestBench {
     public boolean isTouchSensorPressed() {
         return touchSensor.getState();
     }
+
+
 
 
 
